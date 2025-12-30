3699b8c64dbd9a4c02249bb9def6ff6cbab5e94c
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2025-12-30 09:59:00 +0300

    final commit

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -4,6 +4,10 @@ import re
 
 from flask import request
 
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 from models import db, Bakery, BakedGood
 
